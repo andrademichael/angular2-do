b996f53f97aebe7c14eb688b404dda071abc11a6
Michael Andrade and Tracie Weitzman <tracie@>
2017-01-19 09:18:26 -0800

    add ngIf to template to hide edit form until edit button is clicked

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,15 +11,18 @@ import { Component } from '@angular/core';
     </ul>
     <hr>
     <div>
-      <h3>{{selectedTask.description}}</h3>
-      <p>Task Complete? {{selectedTask.done}}</p>
-      <h3>Edit Task</h3>
-      <input [(ngModel)]="selectedTask.description">
-      <label>Enter Task Priority (1-3):</label>
-      <br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+      <div *ngIf="selectedTask">
+        <h3>{{selectedTask.description}}</h3>
+        <p>Task Complete? {{selectedTask.done}}</p>
+        <h3>Edit Task</h3>
+        <input [(ngModel)]="selectedTask.description">
+        <label>Enter Task Priority (1-3):</label>
+        <br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+        <button (click)="finishedEditing()">Done</button>
+      </div>
     </div>
   </div>
   `
@@ -36,7 +39,7 @@ export class AppComponent {
       new Task('Begin brainstorming possible JavaScript group projects', 2),
       new Task('Add README file to last few Angular repos on GitHub', 1)
     ];
-  selectedTask: Task = this.tasks[0];
+  selectedTask = null;
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
@@ -58,6 +61,10 @@ export class AppComponent {
     } else {
       return "bg-success";
     }
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
   }
 }
 
